5ea3171e91676d8c8868b385a374e4c146c22f07
Landy1000 <74451238+Landy1000@users.noreply.github.com>
2021-01-11 00:16:06 +0900

    activeHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shipping_fee
+  belongs_to :shipping_place
+  belongs_to :day
+
   has_one_attached :image
+
+  belongs_to :user
 
   with_options presence: true do
     validates :name
@@ -13,6 +22,12 @@ class Item < ApplicationRecord
     validates :user
     validates :image
   end
-  
-  belongs_to :user
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :shipping_fee_id
+    validates :shipping_place_id
+    validates :day_id
+  end
 end
