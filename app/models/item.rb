6efd65c7d8bcf487509ade6d59848ae23ca9281f
Landy1000 <74451238+Landy1000@users.noreply.github.com>
2021-01-15 22:21:17 +0900

    モデル、バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,21 +13,19 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :explanation
-    validates :category_id
-    validates :state_id
-    validates :shipping_fee_id
-    validates :shipping_place_id
-    validates :day_id
     validates :price
     validates :user
     validates :image
   end
 
-  with_options numericality: { other_than: 1 } do
+  with_options presence: true, numericality: { other_than: 1 , message: 'Select'}  do
     validates :category_id
     validates :state_id
     validates :shipping_fee_id
     validates :shipping_place_id
     validates :day_id
   end
+
+  validates :price, format: { with: /\A[0-9]+\z/ , message: 'Half-width number' }
+  validates :price, numericality: { greater_than: 300, less_than: 9999999 , message: 'Out of setting range'}
 end
