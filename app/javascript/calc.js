b28367a60d34390a236ee3283b7fbfbea808afce
Landy1000 <74451238+Landy1000@users.noreply.github.com>
2021-01-18 16:34:21 +0900

    販売手数料を四捨五入に修正

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -2,7 +2,7 @@ function calc (){
   const priceField = document.getElementById("item-price"); 
 
     const price = priceField.value
-    const fee = price * 0.1
+    const fee = Math.round( price * 0.1 )
     const profit = price - fee
 
     const feeField = document.getElementById('add-tax-price'); 
